3440222727ae363141ddebcf21192b2d61d42c60
MJAISE <mja.ise.1981@gmail.com>
2021-01-14 16:58:28 +0300

    Installing Required Pckages

--- djangoRestAPI/settings.py
+++ djangoRestAPI/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+
 ]
 
 MIDDLEWARE = [
@@ -78,6 +80,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'djangodatabase',
+        # 'USER': 'dbadmin',
+        # 'PASSWORD': '12345',
+        # 'HOST': 'localhost',
+        # 'PORT': '3306',
+
     }
 }
 
